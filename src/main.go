f143f6e990b747058e1ccb1d0034c5e9618f1bc3
Prashant Gupta <prashantgupta@us.ibm.com>
2018-09-26 00:16:12 -0700

    refactoring round 1

--- src/main.go
+++ src/main.go
@@ -15,174 +15,118 @@ import (
 const (
 	//teamURL         = "https://fantasy.premierleague.com/drf/entry/{{.}}/event/4/picks"
 	teamURL         = "https://fantasy.premierleague.com/drf/entry/"
-	playersURL      = "https://fantasy.premierleague.com/drf/bootstrap-static"
+	allPlayersURL   = "https://fantasy.premierleague.com/drf/bootstrap-static"
 	participantsURL = "https://fantasy.premierleague.com/drf/leagues-classic-standings/313?phase=1&le-page=1&ls-page=1"
 )
 
-type fplMain struct {
+type fantasyMain struct {
+	httpClient         *http.Client
 	playerMap          map[int64]string
 	leagueParticipants []int64
 	playerOccurances   map[string]int
 }
-type TeamMainElem struct {
-	TeamPicksElem []TeamPicksElem `json:"picks"`
+
+type ParticipantTeamInfo struct {
+	TeamPlayers []TeamPlayers `json:"picks"`
 }
-type TeamPicksElem struct {
-	Element       int64 `json:"element"`
-	Position      int64 `json:"position"`
-	IsCaptain     bool  `json:"is_captain"`
-	IsViceCaptain bool  `json:"is_vice_captain"`
-	Multiplier    int64 `json:"multiplier"`
+type TeamPlayers struct {
+	Element int64 `json:"element"`
 }
-type PlayerMainElem struct {
-	PlayerElem []PlayerElem `json:"elements"`
+type AllPlayers struct {
+	Players []Players `json:"elements"`
 }
-type PlayerElem struct {
+type Players struct {
 	Id      int64  `json:"id"`
 	WebName string `json:"web_name"`
 }
-
-type Participants struct {
+type LeagueParticipants struct {
 	LeagueStandings LeagueStandings `json:"standings"`
 }
-
 type LeagueStandings struct {
 	LeagueResults []LeagueResults `json:"results"`
 }
-
 type LeagueResults struct {
 	Entry int64 `json:"entry"`
 }
 
-func getTeamInfo(participantNumber int64, fplMain *fplMain) {
-
-	// t := template.New("Participant template")
-
-	// t, err := t.Parse(teamURL)
-	// if err != nil {
-	// 	log.Fatal("Parse: ", err)
-	// 	return
-	// }
-	// // err = t.Execute(os.Stdout, participantNumber)
-	// // if err != nil {
-	// // 	log.Fatal("Execute: ", err)
-	// // 	return
-	// // }
-
-	// fmt.Println("url is ", t)
-
-	var netClient = &http.Client{
-		Timeout: time.Second * 10,
+func makeRequest(fantasyMain *fantasyMain, URL string) []byte {
+	req, err := http.NewRequest(http.MethodGet, URL, nil)
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	req.Header.Set("User-Agent", "pg-fpl")
+
+	resp, err := fantasyMain.httpClient.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+	return body
+}
+
+func getTeamInfoForParticipant(participantNumber int64, fantasyMain *fantasyMain) {
 
 	teamURL := teamURL + strconv.FormatInt(participantNumber, 10) + "/event/6/picks"
-	req, err := http.NewRequest(http.MethodGet, teamURL, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
 
-	req.Header.Set("User-Agent", "pg-fpl")
+	response := makeRequest(fantasyMain, teamURL)
 
-	resp, err := netClient.Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if err != nil {
-		log.Fatal(err)
-	}
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		panic(err.Error())
-	}
-	s := new(TeamMainElem)
-	err = json.Unmarshal(body, &s)
+	ParticipantTeamInfo := new(ParticipantTeamInfo)
+	err := json.Unmarshal(response, &ParticipantTeamInfo)
 	if err != nil {
 		panic(err.Error())
 	}
 
-	for _, player := range s.TeamPicksElem {
-		fplMain.playerOccurances[fplMain.playerMap[player.Element]]++
-		//fmt.Println(fplMain.playerMap[player.Element])
+	for _, player := range ParticipantTeamInfo.TeamPlayers {
+		fantasyMain.playerOccurances[fantasyMain.playerMap[player.Element]]++
 	}
 
 }
 
-func getPlayerMapping(fplMain *fplMain) {
-	var netClient = &http.Client{
-		Timeout: time.Second * 10,
-	}
+func getPlayerMapping(fantasyMain *fantasyMain) {
 
-	req, err := http.NewRequest(http.MethodGet, playersURL, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+	response := makeRequest(fantasyMain, allPlayersURL)
 
-	req.Header.Set("User-Agent", "pg-fpl")
-
-	resp, err := netClient.Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if err != nil {
-		log.Fatal(err)
-	}
-	body, err := ioutil.ReadAll(resp.Body)
+	allPlayers := new(AllPlayers)
+	err := json.Unmarshal(response, &allPlayers)
 	if err != nil {
 		panic(err.Error())
 	}
-	s := new(PlayerMainElem)
-	err = json.Unmarshal(body, &s)
-	if err != nil {
-		panic(err.Error())
-	}
-	for _, player := range s.PlayerElem {
-		fplMain.playerMap[player.Id] = player.WebName
+
+	for _, player := range allPlayers.Players {
+		fantasyMain.playerMap[player.Id] = player.WebName
 	}
 
-	fmt.Println("Fetched " + strconv.Itoa(len(fplMain.playerMap)) + " players")
-	//fmt.Println(fplMain.playerMap[332])
+	fmt.Println("Fetched " + strconv.Itoa(len(fantasyMain.playerMap)) + " players")
 
 }
 
-func getParticipantsInLeague(fplMain *fplMain) {
-	var netClient = &http.Client{
-		Timeout: time.Second * 10,
-	}
+func getParticipantsInLeague(fantasyMain *fantasyMain) {
 
-	req, err := http.NewRequest(http.MethodGet, participantsURL, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+	response := makeRequest(fantasyMain, participantsURL)
 
-	req.Header.Set("User-Agent", "pg-fpl")
-
-	resp, err := netClient.Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if err != nil {
-		log.Fatal(err)
-	}
-	body, err := ioutil.ReadAll(resp.Body)
+	leagueParticipants := new(LeagueParticipants)
+	err := json.Unmarshal(response, &leagueParticipants)
 	if err != nil {
 		panic(err.Error())
 	}
-	s := new(Participants)
-	err = json.Unmarshal(body, &s)
-	if err != nil {
-		panic(err.Error())
-	}
-	for _, participant := range s.LeagueStandings.LeagueResults {
-		fplMain.leagueParticipants = append(fplMain.leagueParticipants, participant.Entry)
+
+	for _, participant := range leagueParticipants.LeagueStandings.LeagueResults {
+		fantasyMain.leagueParticipants = append(fantasyMain.leagueParticipants, participant.Entry)
 	}
 
-	fmt.Println("Fetched " + strconv.Itoa(len(fplMain.leagueParticipants)) + " participants")
+	fmt.Println("Fetched " + strconv.Itoa(len(fantasyMain.leagueParticipants)) + " participants")
 }
 
-func main() {
+func writeToFile(fantasyMain *fantasyMain) {
+
 	file, err := os.Create("result.csv")
 	if err != nil {
 		panic(err)
@@ -191,37 +135,7 @@ func main() {
 
 	writer := csv.NewWriter(file)
 	defer writer.Flush()
-
-	//var data = [][]string{{"abc", 1}, {"Line2", "golangcode.com"}}
-	// data := make(map[string]int)
-
-	// data["a"] = 1
-	// data["b"] = 4
-	// data["c"] = 2
-
-	// for key, value := range data {
-	// 	s := []string{key, strconv.Itoa(value)}
-	// 	err := writer.Write(s)
-	// 	if err != nil {
-	// 		panic(err)
-	// 	}
-	// }
-	fmt.Println("starting main program")
-	fplMain := &fplMain{
-		playerMap:        make(map[int64]string),
-		playerOccurances: make(map[string]int),
-	}
-
-	getPlayerMapping(fplMain)
-
-	getParticipantsInLeague(fplMain)
-	for _, participant := range fplMain.leagueParticipants[0:5] {
-		//fmt.Println("Team ", i)
-		getTeamInfo(participant, fplMain)
-	}
-
-	//sort.(fplMain.playerOccurances)
-	for key, value := range fplMain.playerOccurances {
+	for key, value := range fantasyMain.playerOccurances {
 		//fmt.Println("player: ", key, "Used: ", value)
 		s := []string{string(key), strconv.Itoa(value)}
 		err := writer.Write(s)
@@ -229,4 +143,27 @@ func main() {
 			panic(err)
 		}
 	}
+}
+func main() {
+	fmt.Println("starting main program")
+
+	var httpClient = &http.Client{
+		Timeout: time.Second * 10,
+	}
+
+	fantasyMain := &fantasyMain{
+		httpClient:       httpClient,
+		playerMap:        make(map[int64]string),
+		playerOccurances: make(map[string]int),
+	}
+
+	getPlayerMapping(fantasyMain)
+
+	getParticipantsInLeague(fantasyMain)
+	for _, participant := range fantasyMain.leagueParticipants[0:5] {
+		getTeamInfoForParticipant(participant, fantasyMain)
+	}
+
+	writeToFile(fantasyMain)
+
 }
